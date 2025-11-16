b4b6359686a51e404648da070828c3e07ecd32ff
yeooonn <rladuswjd0303@naver.com>
2025-11-16 19:42:18 +0900

    Updates

--- _next/static/gXlsZtWCwinYOvE61DwVf/_buildManifest.js
+++ _next/static/gXlsZtWCwinYOvE61DwVf/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/yeooonn.github.io//_next/:path+",
+        "destination": "/yeooonn.github.io/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [

9e5d7c607a53d1e4dd67a6aedf100fbc63624b76
yeooonn <rladuswjd0303@naver.com>
2025-11-16 19:34:11 +0900

    feat: layout, page 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sidebar from "./components/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="grid grid-cols-12">
+          <div className="col-span-2 border-r border-[#e0e0e0]">
+            <Sidebar />
+          </div>
+          <div className="col-span-10">{children}</div>
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <h1>포트폴리오</h1>;
+import About from "./pages/About";
+import Contect from "./pages/Contect";
+import Experience from "./pages/Experience";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Skill from "./pages/Skill";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Home />
+      <About />
+      <Skill />
+      <Experience />
+      <Projects />
+      <Contect />
+    </div>
+  );
 }

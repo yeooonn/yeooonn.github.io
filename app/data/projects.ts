ec2626f4da6ca525b4177985e1617a8cb53496e7
yeooonn <rladuswjd0303@naver.com>
2025-11-16 23:28:21 +0900

    feat(Projects): Projects 섹션 데이터 분리 및 동적 렌더링 적용

---
+++ app/data/projects.ts
@@ -0,0 +1,58 @@
+export const projects = [
+  {
+    period: "2025",
+    info: "Link Manage Platform",
+    project: 
+      {
+        title: "LinkKeeper",
+        explanation: '링크 관리 플랫폼',
+        description: [
+          "• 채울 예정",
+          "• 채울 예정",
+          "• 채울 예정",
+          "• 채울 예정",
+        ],
+        skills: ["TypeScript", "React", "Next.js","React-Query", "Styled-Component"],
+        github: 'https://github.com/yeooonn/LinkKeeper',
+        site: 'https://link-keeper-mocha.vercel.app',
+      },
+  },
+  {
+    period: "2025",
+    info: "Study Platform",
+    project: 
+      {
+        title: "MOS Project",
+        explanation: '원스톱 스터디 관리 플랫폼',
+        description: [
+          "• FSD 구조 도입으로 협업 효율 30% 향상",
+          "• Tailwind CSS로 다중 디바이스 반응형 UI 구현",
+          "• React-Hook-Form 기반 프로필 관리 기능",
+          "• Next.js + React-Query 일정 캘린더 CRUD 최적화",
+          "• 좋아요 기능, 공지사항 DnD 기능 개발",
+        ],
+        skills: ["TypeScript", "Next.js", "React-Query", "TailwindCSS"],
+        github: 'https://github.com/project-mos/frontend',
+        site: '',
+      },
+  },
+  {
+    period: "2024",
+    info: "AI Interview",
+    project: 
+      {
+        title: "Sulsul",
+        explanation: 'AI 기반 취업 면접 지원 플랫폼',
+        description: [
+          "• 100문항 SSG 렌더링으로 초기 로딩 속도 개선",
+          "• AI 피드백 UI 플로우 설계 및 구현",
+          "• 진행률 기반 캐릭터 인터랙션 및 Stepper 컴포넌트 개발",
+          "• Skeleton UI 적용으로 로딩 UX 개선",
+          "• 데스크탑, 태블릿, 모바일 반응형 웹 구현",
+        ],
+        skills: ["TypeScript", "Next.js", "React-Query", "zustand"],
+        github: 'https://github.com/sulsulsulsul/sulsul',
+        site: '',
+      },
+  },
+];
--- app/pages/Projects.tsx
+++ app/pages/Projects.tsx
@@ -1,7 +1,77 @@
+import SkillBox from "../components/SkillBox";
+import Typography from "../components/Typography";
+import { projects } from "../data/projects";
+
 const Projects = () => {
   return (
-    <section id="Projects" className="w-full h-screen border border-red-500">
-      <h1>Projects</h1>
+    <section
+      id="Projects"
+      className="w-full min-h-screen h-auto px-30 py-20 flex items-center"
+    >
+      <div className="w-full flex flex-col gap-7">
+        <div className="mb-7">
+          <h1>05</h1>
+          <Typography.Head2>Projects</Typography.Head2>
+        </div>
+
+        {projects.map((exp, idx) => (
+          <div key={idx}>
+            <div className="text-xl font-light grid grid-cols-12">
+              <div className="2xl:col-span-3 col-span-2">
+                <Typography.P3 className="leading-relaxed text-[#999]">
+                  {exp.period}
+                </Typography.P3>
+                <Typography.P3 className="leading-relaxed">
+                  {exp.info}
+                </Typography.P3>
+              </div>
+
+              <div className="2xl:col-span-4 col-span-5 flex flex-col gap-2">
+                <Typography.P1 className="!text-3xl">
+                  {exp.project.title}
+                </Typography.P1>
+                <Typography.P3 className="font-medium mb-4">
+                  {exp.project.explanation}
+                </Typography.P3>
+                {exp.project.description.map((desc, dIdx) => (
+                  <Typography.P3 key={dIdx} className="font-normal text-[#999]">
+                    {desc}
+                  </Typography.P3>
+                ))}
+                <div className="flex gap-3 flex-wrap mt-4">
+                  {exp.project.skills.map((skill, sIdx) => (
+                    <SkillBox key={sIdx} text={skill} />
+                  ))}
+                </div>
+
+                <div className="flex gap-6 mt-2 relative z-10">
+                  {exp.project.github !== "" && (
+                    <a href={exp.project.github} target="_blank">
+                      <Typography.P3 className="border-b border-blue-500 text-blue-500">
+                        Github →
+                      </Typography.P3>
+                    </a>
+                  )}
+                  {exp.project.site !== "" && (
+                    <a href={exp.project.site} target="_blank">
+                      <Typography.P3 className="border-b border-blue-500 text-blue-500">
+                        Visit Site →
+                      </Typography.P3>
+                    </a>
+                  )}
+                </div>
+              </div>
+              <div className="2xl:col-span-5 col-span-5 h-auto border border-gray-400 flex justify-center items-center">
+                이미지 들어갈 예정
+              </div>
+            </div>
+
+            {projects.length !== idx + 1 && (
+              <hr className="border border-gray-200 my-10" />
+            )}
+          </div>
+        ))}
+      </div>
     </section>
   );
 };

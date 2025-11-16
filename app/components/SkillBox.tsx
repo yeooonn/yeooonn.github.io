799446729c68959466a5e9ba59a540b2092205e1
yeooonn <rladuswjd0303@naver.com>
2025-11-16 22:13:17 +0900

    feat(SkilBox): 스타일 수정 및 text prop 추가

--- app/components/SkillBox.tsx
+++ app/components/SkillBox.tsx
@@ -1,5 +1,11 @@
-const SkillBox = () => {
-  return <div className="border border-gray-500 w-auto">TypeScript</div>;
+import Typography from "./Typography";
+
+const SkillBox = ({ text }: { text: string }) => {
+  return (
+    <div className="border border-gray-300 w-auto self-start px-3 py-0.5 rounded-sm">
+      <Typography.P3 className="text-gray-500">{text}</Typography.P3>
+    </div>
+  );
 };
 
 export default SkillBox;

0c3dbb879ec9f18a7f56a33ba7351b4955fb2e9e
yeooonn <rladuswjd0303@naver.com>
2025-11-16 23:28:45 +0900

    feat(Contect): UI 개발

--- app/pages/Contect.tsx
+++ app/pages/Contect.tsx
@@ -1,7 +1,22 @@
+import Typography from "../components/Typography";
+
 const Contect = () => {
   return (
-    <section id="Contect" className="w-full h-screen border border-red-500">
-      <h1>Contect</h1>
+    <section
+      id="Contect"
+      className="w-full h-screen bg-gray-50 px-30 flex items-center"
+    >
+      <div className="flex flex-col gap-7">
+        <div className="mb-20">
+          <h1>06</h1>
+          <Typography.Head2>Contect</Typography.Head2>
+        </div>
+
+        <div>
+          <Typography.P1 className="text-gray-400">Email</Typography.P1>
+          <Typography.P2>yeooonn0303@naver.com</Typography.P2>
+        </div>
+      </div>
     </section>
   );
 };

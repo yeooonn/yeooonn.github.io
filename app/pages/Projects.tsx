c7cd5e74e7ddec40838d8e36ad912ed50c2b6a0b
yeooonn <rladuswjd0303@naver.com>
2025-12-19 21:52:15 +0900

    feat: 프로젝트 섹션 레이아웃 수정

--- app/pages/Projects.tsx
+++ app/pages/Projects.tsx
@@ -16,8 +16,72 @@ const Projects = () => {
 
         {projects.map((exp, idx) => (
           <div key={idx}>
-            <div className="text-xl font-light grid grid-cols-12">
-              <div className="2xl:col-span-3 col-span-2">
+            <div className="mb-1">
+              <Typography.P3 className="leading-relaxed text-[#999]">
+                {exp.period}
+              </Typography.P3>
+              <Typography.P1 className="!text-3xl">
+                {exp.project.title}
+              </Typography.P1>
+              <div className="flex justify-between items-center">
+                <Typography.P2 className="font-medium mb-4 text-[#999] w-1/2">
+                  {exp.project.explanation}
+                </Typography.P2>
+                <Typography.P2 className="font-medium mb-4 w-1/2">
+                  [ 구현사항 ]
+                </Typography.P2>
+              </div>
+            </div>
+
+            <div className="text-xl font-light grid grid-cols-12 mb-10">
+              <div className="col-span-6 flex flex-col gap-2 mr-10">
+                <div className="h-70 border border-gray-400 flex justify-center items-center">
+                  이미지 들어갈 예정
+                </div>
+
+                <div className="flex gap-6 mt-2 relative z-10">
+                  {exp.project.github !== "" && (
+                    <a href={exp.project.github} target="_blank">
+                      <Typography.P3 className="border-b border-blue-500 text-blue-500">
+                        Github →
+                      </Typography.P3>
+                    </a>
+                  )}
+                  {exp.project.site !== "" && (
+                    <a href={exp.project.site} target="_blank">
+                      <Typography.P3 className="border-b border-blue-500 text-blue-500">
+                        Visit Site →
+                      </Typography.P3>
+                    </a>
+                  )}
+                </div>
+              </div>
+
+              <div className="col-span-6 flex flex-col gap-2">
+                {exp.project.description.map((desc, dIdx) => (
+                  <Typography.P2 key={dIdx} className="font-normal text-[#999]">
+                    {desc}
+                  </Typography.P2>
+                ))}
+                <div className="flex gap-3 flex-wrap mt-4">
+                  {exp.project.skills.map((skill, sIdx) => (
+                    <SkillBox key={sIdx} text={skill} />
+                  ))}
+                </div>
+              </div>
+            </div>
+
+            {projects.length !== idx + 1 && (
+              <hr className="border border-gray-200 my-10" />
+            )}
+          </div>
+        ))}
+      </div>
+
+      {/* {projects.map((exp, idx) => (
+          <div key={idx}>
+            <div className="text-xl font-light grid grid-cols-12 mb-10">
+              <div className="2xl:col-span-6 col-span-3">
                 <Typography.P3 className="leading-relaxed text-[#999]">
                   {exp.period}
                 </Typography.P3>
@@ -26,7 +90,7 @@ const Projects = () => {
                 </Typography.P3>
               </div>
 
-              <div className="2xl:col-span-4 col-span-5 flex flex-col gap-2">
+              <div className="2xl:col-span-6 col-span-9 flex flex-col gap-2">
                 <Typography.P1 className="!text-3xl">
                   {exp.project.title}
                 </Typography.P1>
@@ -61,9 +125,9 @@ const Projects = () => {
                   )}
                 </div>
               </div>
-              <div className="2xl:col-span-5 col-span-5 h-auto border border-gray-400 flex justify-center items-center">
-                이미지 들어갈 예정
-              </div>
+            </div>
+            <div className="h-100 border border-gray-400 flex justify-center items-center">
+              이미지 들어갈 예정
             </div>
 
             {projects.length !== idx + 1 && (
@@ -71,7 +135,7 @@ const Projects = () => {
             )}
           </div>
         ))}
-      </div>
+      </div> */}
     </section>
   );
 };
